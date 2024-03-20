8cc3ecb6f006f283940ed6e891117c88216841c4
EnigmaWPD <36955267+EnigmaWPD@users.noreply.github.com>
2024-03-20 10:35:37 +0000

    get_restricted_dict tweak
    - Condition added to get_restricted_dict to use a different implementation if the extension ckanext-scheming is not being used in this currently running CKAN site instance

--- ckanext/restricted_api/util.py
+++ ckanext/restricted_api/util.py
@@ -8,6 +8,7 @@ from logging import getLogger
 import ckan.logic as logic
 from ckan.model import User
 from ckan.plugins import toolkit
+from ckan.lib import app_globals
 
 log = getLogger(__name__)
 
@@ -113,33 +114,44 @@ def get_restricted_dict(resource_dict):
     The ckan plugin ckanext-scheming changes the structure of the resource
     dict and the nature of how to access our restricted field values.
     """
-    restricted_dict = {"level": "public", "allowed_users": []}
+    ckanext_scheming_exists = 'ckanext-scheming' in app_globals.get_globals_key("ckan.plugins")
+    if ckanext_scheming_exists:
+        restricted_dict = {"level": "public", "allowed_users": []}
 
-    if resource_dict:
-        # the dict might exist as a child inside the extras dict
-        extras = resource_dict.get("extras", {})
-        # or the dict might exist as a direct descendant of the resource dict
-        restricted = resource_dict.get("restricted", extras.get("restricted", {}))
-        if not isinstance(restricted, dict):
-            # if the restricted property does exist, but not as a dict,
-            # we may need to parse it as a JSON string to gain access to the values.
-            # as is the case when making composite fields
-            try:
-                restricted = json.loads(restricted)
-            except ValueError:
-                restricted = {}
+        if resource_dict:
+            # the dict might exist as a child inside the extras dict
+            extras = resource_dict.get("extras", {})
+            # or the dict might exist as a direct descendant of the resource dict
+            restricted = resource_dict.get("restricted", extras.get("restricted", {}))
+            if not isinstance(restricted, dict):
+                # if the restricted property does exist, but not as a dict,
+                # we may need to parse it as a JSON string to gain access to the values.
+                # as is the case when making composite fields
+                try:
+                    restricted = json.loads(restricted)
+                except ValueError:
+                    restricted = {}
 
-        if restricted:
-            restricted_level = restricted.get("level", "public")
-            allowed_users = restricted.get("allowed_users", [])
-            if not isinstance(allowed_users, list):
-                allowed_users = allowed_users.split(",")
-            restricted_dict = {
-                "level": restricted_level,
-                "allowed_users": allowed_users,
-            }
+            if restricted:
+                restricted_level = restricted.get("level", "public")
+                allowed_users = restricted.get("allowed_users", [])
+                if not isinstance(allowed_users, list):
+                    allowed_users = allowed_users.split(",")
+                restricted_dict = {
+                    "level": restricted_level,
+                    "allowed_users": allowed_users,
+                }
 
-    return restricted_dict
+        return restricted_dict
+
+    # @see https://github.com/olivierdalang/ckanext-restricted/commit/89693f5e4a2a4dedf2cada289d1bf46bd7991069
+    restricted_level = resource_dict.get('restricted_level', 'public')
+    allowed_users = resource_dict.get('restricted_allowed_users', '')
+    if not isinstance(allowed_users, list):
+        allowed_users = allowed_users.split(',')
+    return {
+        'level': restricted_level,
+        'allowed_users': allowed_users}
 
 
 def check_user_resource_access(user, resource_dict, package_dict):
